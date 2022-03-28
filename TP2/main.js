359c2ad9e923a855db60200da0475b4d8878f58d
Ginyto <agianlucca@gmail.com>
2022-03-28 21:35:36 +0200

    exercice 4 done manque plus que l'exo 5

--- TP2/main.js
+++ TP2/main.js
@@ -23,11 +23,51 @@ const vm2 = Vue.createApp({
     },
 
     methods: {
+
         inverser() {
             this.isWhite = !this.isWhite
             console.log(this.isWhite)
+        },
+    },
+
+    computed: {
+
+        myText() {
+            if (this.isWhite) {
+                return 'Blanc'
+            }
+            return 'Noir'
         }
     }
 })
 
 vm2.mount('#vm2')
+
+
+
+const tableau = Vue.createApp({
+
+    data() {
+        return {
+
+            tab:[
+                { nom : "Dupont", note : 12, groupe: "A" },
+                { nom : "Aubin", note : 8, groupe : "B" },
+                { nom : "François", note : 14, groupe : "B" },
+                { nom: "Martin", note: 9, groupe: "A" }
+            ]
+        }
+    },
+
+    methods: {
+        quelgroupe(i) {
+            if (this.tab[i].groupe === "A") {
+                return true
+            }
+            return false
+            }
+        }
+    
+})
+
+tableau.mount('#tableau')
