c9631ca4e613a4bb2a23fbb1a452b5695c900bc5
Ginyto <agianlucca@gmail.com>
2022-04-04 20:07:50 +0200

    je reprend le taff

--- TP3/todo/src/components/Exo1.vue
+++ TP3/todo/src/components/Exo1.vue
@@ -1,26 +1,44 @@
 <template>
 
     <div>
-        <h1>Ceci est notre première app en vue JS en voici le titre -> {{message}}</h1>
+        <h1 id="titre">Ceci est notre première app en vue JS en voici le titre -> {{message}}</h1>
+
+        <Todo></Todo>
+
+
     </div>
 
 </template>
 
 <script>
+
+    import Todo from './Todo.vue'
+
     export default {
         name: 'Exo-1',
+
         data() {
             return {
                 message: 'Hello Vue!',
             }
         },
+
         methods: {
+
         },
         computed: {
+
         },
+
+        components: {
+            Todo
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 
+#titre{
+    background-color: #a9ff38;
+}
 </style>
---
+++ TP3/todo/src/components/Todo.vue
@@ -0,0 +1,59 @@
+<template>
+
+    <div id="main">
+        <h1 id = "titre">TODO LIST</h1>
+
+        <div id="todo">
+
+            Titre de la tache <br>
+            <input type="text" class="saisie">
+
+            <br>
+
+            Description de la tache <br>
+            <input type="text" class="saisie">
+
+            <br>
+
+            <button type='button'>Ajouter la tâche</button>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'todo-list',
+
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+    }
+
+</script>
+
+
+<style lang="css" scoped>
+
+#main{
+    background-color: #e89eff;
+}
+
+#todo{
+    background-color: #ffd6a9;
+
+}
+
+
+</style>
